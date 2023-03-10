931db00bce95bb2357c4d9c9530cd5e8377b76d9
Rayane Merlin <92400806+rayanemrln@users.noreply.github.com>
2023-03-10 02:47:02 +0100

    update SQL

--- asset/sql/timechat.sql
+++ asset/sql/timechat.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : jeu. 09 mars 2023 à 22:49
+-- Généré le : ven. 10 mars 2023 à 01:46
 -- Version du serveur : 8.0.31
 -- Version de PHP : 8.2.0
 
@@ -29,11 +29,10 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `friends`;
 CREATE TABLE IF NOT EXISTS `friends` (
-  `id` int NOT NULL AUTO_INCREMENT,
   `id_user_1` int NOT NULL,
   `id_user_2` int NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_user_1` (`id_user_1`)
+  PRIMARY KEY (`id_user_1`,`id_user_2`),
+  KEY `id_user_2` (`id_user_2`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- --------------------------------------------------------
@@ -99,6 +98,21 @@ CREATE TABLE IF NOT EXISTS `message` (
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `notification`
+--
+
+DROP TABLE IF EXISTS `notification`;
+CREATE TABLE IF NOT EXISTS `notification` (
+  `id_user_sender` int NOT NULL,
+  `id_user_receiver` int NOT NULL,
+  `type` varchar(30) NOT NULL,
+  `viewed` tinyint(1) NOT NULL DEFAULT '0',
+  `date` datetime NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `profile_theme`
 --
 
@@ -116,9 +130,7 @@ CREATE TABLE IF NOT EXISTS `profile_theme` (
 --
 
 INSERT INTO `profile_theme` (`id_user`, `theme`, `banner`) VALUES
-(26, 'violet', 'HoudaKadous-03-09-23-992.jpg'),
-(25, 'violet', 'NapsDeMarseille-03-09-23-75.jpg'),
-(24, 'orange', 'root-03-09-23-108.png');
+(3, 'red', 'root-03-10-23-796.jpg');
 
 -- --------------------------------------------------------
 
@@ -137,19 +149,18 @@ CREATE TABLE IF NOT EXISTS `user` (
   `is_public` tinyint(1) NOT NULL,
   `is_connected` tinyint(1) NOT NULL DEFAULT '0',
   `nb_friends` int NOT NULL DEFAULT '0',
+  `qrcode` varchar(40) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `pseudo` (`username`),
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
 
 --
 -- Déchargement des données de la table `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `password`, `description`, `profile_picture`, `signup_at`, `is_public`, `is_connected`, `nb_friends`) VALUES
-(24, 'root', '$2y$10$lVaO2ImYJN16KrxhS6bQHeRFa1IrihtW.kyAil4Sed5Any8afanyy', 'Admin et grand crack de cette génération', 'root-03-09-23-52.jpg', '2023-03-08', 0, 0, 0),
-(25, 'NapsDeMarseille', '$2y$10$YoR2QsdnRwPz0.P6uoKtiuUHZKe9hgMLB2alTWbUEHsBCzLIQVPhe', 'Okay okay c\'est naps khapta vers le port GAMBERGEE okay', 'NapsDeMarseille-03-09-23-808.jpg', '2023-03-08', 0, 0, 0),
-(26, 'Houda', '$2y$10$nNSgdvf1zsko33Exe85zFusAQ/Sxutw2flcKZZP0.5GwSCPHfj2sy', 'Bonsoir', 'HoudaKadous-03-09-23-900.png', '2023-03-09', 1, 0, 0);
+INSERT INTO `user` (`id`, `username`, `password`, `description`, `profile_picture`, `signup_at`, `is_public`, `is_connected`, `nb_friends`, `qrcode`) VALUES
+(3, 'root', '$2y$10$8tCOiWueqsx.Lf/dUz6pcOjgpzwhGtXat3.6sbXP4ThhwHH.lUUqe', 'Admin et grand crack de cette génération', 'root-03-10-23-898.jpg', '2023-03-10', 0, 0, 0, '3-640a8b3a259b4.png');
 
 --
 -- Contraintes pour les tables déchargées
@@ -159,7 +170,8 @@ INSERT INTO `user` (`id`, `username`, `password`, `description`, `profile_pictur
 -- Contraintes pour la table `friends`
 --
 ALTER TABLE `friends`
-  ADD CONSTRAINT `friends_ibfk_1` FOREIGN KEY (`id_user_1`) REFERENCES `user` (`id`);
+  ADD CONSTRAINT `friends_ibfk_1` FOREIGN KEY (`id_user_1`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `friends_ibfk_2` FOREIGN KEY (`id_user_2`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
 -- Contraintes pour la table `message`
